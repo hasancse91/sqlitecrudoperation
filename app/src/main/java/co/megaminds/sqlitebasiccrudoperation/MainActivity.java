eb879e55f2e8dbd704f7f7753f14760926bb6a25
hasan_cse91 <hasan_cse91@yahoo.com>
2016-09-22 09:38:12 +0600

    input field added

--- app/src/main/java/co/megaminds/sqlitebasiccrudoperation/MainActivity.java
+++ app/src/main/java/co/megaminds/sqlitebasiccrudoperation/MainActivity.java
@@ -1,13 +1,20 @@
 package co.megaminds.sqlitebasiccrudoperation;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +29,52 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                showInputDialog();
             }
         });
+    }
+
+    private void showInputDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Input Dialog Box");
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText nameEditText = new EditText(this);
+        nameEditText.setHint("Name");
+        layout.addView(nameEditText);
+
+        final EditText phoneEditText = new EditText(this);
+        phoneEditText.setHint("Phone Number");
+        layout.addView(phoneEditText);
+
+        alert.setView(layout);
+
+        alert.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                String nameString = nameEditText.getText().toString();
+                String phoneString = phoneEditText.getText().toString();
+
+                saveInformationToDatabase(nameString, phoneString);
+
+                Toast.makeText(getApplicationContext(), nameString + "\n" + phoneString, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // what ever you want to do with No option.
+            }
+        });
+
+        alert.show();
+    }
+
+    private void saveInformationToDatabase(String name, String phone) {
+
     }
 
     @Override
